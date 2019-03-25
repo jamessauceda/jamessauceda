3ff4965d8537845de949d762d195f4ccf1c61f7f
James Sauceda <jamesxchance@gmail.com>
2019-03-24 17:29:48 -0700

    weeding

---
+++ wedding/gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// compile scss to css
+gulp.task('sass', function() {
+  return gulp
+    .src('./sass/styles.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(rename({ basename: 'styles.min' }))
+    .pipe(gulp.dest('./css'));
+});
+
+// watch changes in scss files and run sass task
+gulp.task('sass:watch', function() {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+// minify js
+gulp.task('minify-js', function() {
+  return gulp
+    .src('./js/scripts.js')
+    .pipe(uglify())
+    .pipe(rename({ basename: 'scripts.min' }))
+    .pipe(gulp.dest('./js'));
+});
+
+gulp.task('scripts', function() {
+  return gulp
+    .src('js/*.js')
+    .pipe(
+      uglify().on('error', function(e) {
+        console.log(e);
+      }))
+    .pipe(gulp.dest('minjs'));
+});
+
+// default task
+gulp.task('default', ['sass', 'minify-js']);
